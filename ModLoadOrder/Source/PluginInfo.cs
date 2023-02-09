b50332751ae37166e6e04ed399c31071fcbc2774
Tim <X_Blaster@panhandle.rr.com>
2023-02-08 22:17:48 -0600

    Workshop ID fixup

--- ModLoadOrder/Source/PluginInfo.cs
+++ ModLoadOrder/Source/PluginInfo.cs
@@ -13,7 +13,7 @@ namespace ModLoadOrder.Source
 		public const string PLUGIN_SHORTNAME = "MLOrder";
 		public const string PLUGIN_VERSION   = "1.0.0";
 		// This is your workshop id for the mod.
-		public const uint   PLUGIN_ID        = 0;
+		public const uint   PLUGIN_ID        = 2930592907;
 		// This is what Harmony uses to distinguish different plugins and patchers from each other.
 		// This string can really be anything, but this format is the most used and easy to recognise.
 		public const string PLUGIN_GUID      = "org.stationeers." + PLUGIN_AUTHOR + "." + PLUGIN_SHORTNAME;
